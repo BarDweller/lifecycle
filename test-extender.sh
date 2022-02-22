9b60f8bf953f3922c83fcdf98a811b965b9a46ed
Ozzy Osborne <bardweller@gmail.com>
2022-02-22 16:00:58 -0500

    Add ubi8 test

--- test-extender.sh
+++ test-extender.sh
@@ -9,6 +9,14 @@ if [ -z "$REGISTRY_HOST" ]; then
 fi
 echo "REGISTRY_HOST: $REGISTRY_HOST"
 
+if [ -z "$TESTDATA" ]; then
+  TESTDATA="testdata"
+fi
+echo "TESTDATA: $TESTDATA"
+echo "RUN_IMAGE: $RUN_IMAGE"
+
+echo ">>>>>>>>>> Cleanup old images"
+
 # Remove output images from daemon - note that they STILL EXIST in the local registry
 docker image rm $REGISTRY_HOST/test-builder --force
 docker image rm $REGISTRY_HOST/extended/runimage --force   # run image to extend
@@ -19,18 +27,9 @@ echo ">>>>>>>>>> Building lifecycle..."
 make clean build-linux-amd64
 cd $LIFECYCLE_REPO_PATH/out/linux-amd64
 
-# SKIP_BUILD_IMAGE is needed if you want to test build cache being used in subsequent build extension runs
-if [ -z "$SKIP_BUILD_IMAGE" ]; then
-  docker image rm $REGISTRY_HOST/extended/buildimage --force # build image to extend
-  echo ">>>>>>>>>> Building build base image..."
+echo ">>>>>>>>>> Create images"
 
-  cat <<EOF >Dockerfile
-  FROM cnbs/sample-builder:bionic
-  COPY ./lifecycle /cnb/lifecycle
-EOF
-  docker build -t $REGISTRY_HOST/test-builder .
-  docker push $REGISTRY_HOST/test-builder
-fi
+source $LIFECYCLE_REPO_PATH/extender/$TESTDATA/images/create_images.sh
 
 echo ">>>>>>>>>> Building extender minimal image..."
 
@@ -43,8 +42,7 @@ docker build -f Dockerfile.extender -t $REGISTRY_HOST/extender .
 docker push $REGISTRY_HOST/extender
 
 echo ">>>>>>>>>> Preparing fixtures..."
-
-FIXTURES_PATH=$LIFECYCLE_REPO_PATH/extender/testdata
+FIXTURES_PATH=$LIFECYCLE_REPO_PATH/extender/$TESTDATA
 cd $FIXTURES_PATH
 
 rm -rf ./kaniko
@@ -60,6 +58,7 @@ docker run \
   -v $PWD/layers/:/layers \
   -v $PWD/platform/:/platform \
   -v $PWD/workspace/:/workspace \
+  --user 1000:1000 \
   $REGISTRY_HOST/test-builder \
   /cnb/lifecycle/detector -order /layers/order.toml -log-level debug
 
@@ -71,6 +70,7 @@ docker run \
   -v $PWD/layers/:/layers \
   -v $PWD/platform/:/platform \
   -v $PWD/workspace/:/workspace \
+  --user 1000:1000 \
   $REGISTRY_HOST/test-builder \
   /cnb/lifecycle/builder -use-extensions -log-level debug
 
@@ -87,7 +87,7 @@ docker run \
   -v $PWD/layers/:/layers \
   -v $PWD/workspace/:/workspace \
   -e REGISTRY_HOST=$REGISTRY_HOST \
-  -u root \
+  --user 0:0 \
   --network host \
   $REGISTRY_HOST/extender \
   /cnb/lifecycle/extender \
@@ -102,7 +102,7 @@ docker run \
 
 docker pull $REGISTRY_HOST/extended/buildimage
 
-echo ">>>>>>>>>> Running extend on run image..."
+echo ">>>>>>>>>> Running extend on run image... $RUN_IMAGE"
 
 docker run \
   -v $PWD/cnb/ext/:/cnb/ext \
@@ -110,7 +110,7 @@ docker run \
   -v $PWD/layers/:/layers \
   -v $PWD/workspace/:/workspace \
   -e REGISTRY_HOST=$REGISTRY_HOST \
-  -u root \
+  --user 0:0 \
   --network host \
   $REGISTRY_HOST/extender \
   /cnb/lifecycle/extender \
@@ -120,7 +120,7 @@ docker run \
   -kind run \
   -log-level debug \
   -work-dir /kaniko \
-  cnbs/sample-stack-run:bionic \
+  $RUN_IMAGE \
   $REGISTRY_HOST/extended/runimage
 
 docker pull $REGISTRY_HOST/extended/runimage
@@ -133,14 +133,14 @@ docker run \
   -v $PWD/layers/:/layers \
   -v $PWD/platform/:/platform \
   -v $PWD/workspace/:/workspace \
-  -u root \
+  --user 0:0 \
   --network host \
   $REGISTRY_HOST/test-builder \
   /cnb/lifecycle/exporter -log-level debug -run-image $REGISTRY_HOST/extended/runimage $REGISTRY_HOST/appimage
 
 docker pull $REGISTRY_HOST/appimage
 
-echo ">>>>>>>>>> Validating app image..."
+echo ">>>>>>>>>> Validate app image"
 
-docker run --rm --entrypoint cat -it $REGISTRY_HOST/appimage /opt/arg.txt
-docker run --rm --entrypoint curl -it $REGISTRY_HOST/appimage google.com
+source $LIFECYCLE_REPO_PATH/extender/$TESTDATA/images/validate_run_image.sh
+
